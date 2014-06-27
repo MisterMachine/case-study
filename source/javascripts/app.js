992bb2abc22bbf2bce996ad4429f525e6d490641
menslow <michael.enslow@gmail.com>
2014-06-26 18:49:01 -0700

    added jquery nav scroll event

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,20 +1,45 @@
-$(document).foundation();
+var CS = (function() {
 
-$(document).ready(function() {
-  
-  $('body').addClass('js');
-  
-  var $menu = $('#menu'),
-    $menulink = $('.menu-link'),
-    $control = $('.control'),
-    $canvas = $('.slide-canvas'),
-    $container = $('.container');
-  
-  $menulink.click(function() {
-    $menulink.toggleClass('active');
-    $control.toggleClass('active');
-    $canvas.toggleClass('active');
-    $container.toggleClass('active');
-    return false;
-  });
+  var $ = jQuery;
+
+  return {
+
+    init: function() {
+      this.setFoundation();
+      this.setJS();
+      this.setMenuEvents();
+      this.setPageScroll();
+    },
+
+    setFoundation: function() {
+      $(document).foundation();
+    },
+
+    setJS: function() {
+      $('body').addClass('js');
+    },
+
+    setMenuEvents: function() {
+
+    },
+
+    setPageScroll: function() {
+      // Add click event with animated scroll
+      $('ul.scroll a').on('click', function(e){
+        e.preventDefault();
+
+        var speed = 400,
+           anchor = $(this).attr('href');
+           loc = $(anchor).offset().top;
+
+        $('body').animate( { scrollTop: loc }, speed );
+      });
+    }
+
+  };
+} () );
+
+jQuery(document).ready(function() {
+  CS.init();
 });
+
